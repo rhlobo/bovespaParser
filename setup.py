d8f2ed50e8fbb40d75c2dc5cef3d5b5bf99d1127
rhlobo <rhlobo+github@gmail.com>
2014-12-26 18:22:19 -0200

    Updated setup script

--- setup.py
+++ setup.py
@@ -2,36 +2,32 @@
 # Filename: setup.py
 
 
-from setuptools import setup, find_packages
 import os
+from setuptools import setup, find_packages
 
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-setup(
-    name="bovespaparser",
-    version="0.5",
-    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-    author="Roberto Haddock Lobo",
-    author_email="rhlobo+stockexperiments@gmail.com",
-    description="Bovespa's historical series files parser.",
-    long_description=read('README.md'),
-    license="MIT",
-    test_suite='bovespaparser.tests.bovespaparser_tests.TestBovespaParserFunctions',
-    url='http://pypi.python.org/pypi/bovespaparser',
-    keywords=["bovespa", "parser", "historical", "series", "cotahist", "stock"],
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Development Status :: 4 - Beta",
-        "Environment :: Other Environment",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Financial and Insurance Industry",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Office/Business :: Financial :: Investment",
-    ],
-)
+setup(name="bovespaparser",
+      version="0.5",
+      packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+      author="Roberto Haddock Lobo",
+      author_email="rhlobo+stockexperiments@gmail.com",
+      description="Bovespa's historical series files parser.",
+      long_description=read('README.md'),
+      license="MIT",
+      test_suite='bovespaparser.tests.bovespaparser_tests.TestBovespaParserFunctions',
+      url='http://pypi.python.org/pypi/bovespaparser',
+      keywords=["bovespa", "parser", "historical", "series", "cotahist", "stock"],
+      classifiers=["Programming Language :: Python",
+                   "Programming Language :: Python :: 2.7",
+                   "Development Status :: 4 - Beta",
+                   "Environment :: Other Environment",
+                   "Intended Audience :: Developers",
+                   "Intended Audience :: Financial and Insurance Industry",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent",
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   "Topic :: Office/Business :: Financial :: Investment"])
